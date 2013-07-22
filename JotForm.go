753816596dd2537dd704eba40dd9e5b56386e5a3
ceren <ceren@jotform.com>
2013-07-22 11:05:53 +0300

    client constructor added

--- JotForm.go
+++ JotForm.go
@@ -14,22 +14,27 @@ import(
 const baseURL = "http://api.jotform.com"
 const apiVersion = "v1"
 
-type JotformAPIClient struct{
-    Username string
-    ApiKey string
+type jotformAPIClient struct{
+    apiKey string
 }
 
-func (client JotformAPIClient) checkClient() {
-    if client.ApiKey == "" {
+func NewJotFormAPIClient(apiKey string) *jotformAPIClient {
+    client := &jotformAPIClient{apiKey}
+
+    return client
+}
+
+func (client jotformAPIClient) checkClient() {
+    if client.apiKey == "" {
         fmt.Printf("ApiKey is requied!\n")
         os.Exit(1)
     }
 }
 
-func (client JotformAPIClient) getRequest(requestPath string, params string) []byte {
+func (client jotformAPIClient) getRequest(requestPath string, params string) []byte {
     client.checkClient()
     
-    var path = baseURL + "/" + apiVersion + "/" + requestPath + "?" + params + "apiKey=" + client.ApiKey
+    var path = baseURL + "/" + apiVersion + "/" + requestPath + "?" + params + "apiKey=" + client.apiKey
 
     response, err := http.Get(path)
 
@@ -58,10 +63,10 @@ func (client JotformAPIClient) getRequest(requestPath string, params string) []b
     return nil
 }
 
-func (client JotformAPIClient) postRequest(requestPath string, params map[string]string) [] byte {
+func (client jotformAPIClient) postRequest(requestPath string, params map[string]string) [] byte {
     client.checkClient()
-    
-    var path = baseURL + "/" + apiVersion + "/" + requestPath + "?apiKey=" + client.ApiKey
+
+    var path = baseURL + "/" + apiVersion + "/" + requestPath + "?apiKey=" + client.apiKey
 
     values := make(url.Values)
 
@@ -96,10 +101,10 @@ func (client JotformAPIClient) postRequest(requestPath string, params map[string
     return nil
 }
 
-func (client JotformAPIClient) deleteRequest(requestPath string) [] byte {
+func (client jotformAPIClient) deleteRequest(requestPath string) [] byte {
     client.checkClient()
 
-    var path = "http://api.jotform.com/v1/" + requestPath + "?apiKey=" + client.ApiKey  
+    var path = "http://api.jotform.com/v1/" + requestPath + "?apiKey=" + client.apiKey  
 
     request, err := http.NewRequest("DELETE", path, nil)
 
@@ -166,65 +171,65 @@ func createConditions (offset string, limit string, filter map[string]string, or
     return params
 }
 
-func (client JotformAPIClient) GetUser() []byte {
+func (client jotformAPIClient) GetUser() []byte {
     return client.getRequest("user", "")
 }
 
-func (client JotformAPIClient) GetUsage() []byte {
+func (client jotformAPIClient) GetUsage() []byte {
     return client.getRequest("user/usage", "")
 }
 
-func (client JotformAPIClient) GetForms(offset string, limit string, filter map[string]string, orderBy string) []byte {
+func (client jotformAPIClient) GetForms(offset string, limit string, filter map[string]string, orderBy string) []byte {
     var params = createConditions(offset, limit, filter, orderBy)
 
     return client.getRequest("user/forms", params)
 }
 
-func (client JotformAPIClient) GetSubmissions(offset string, limit string, filter map[string]string, orderBy string) []byte {
+func (client jotformAPIClient) GetSubmissions(offset string, limit string, filter map[string]string, orderBy string) []byte {
     var params = createConditions(offset, limit, filter, orderBy)
 
     return client.getRequest("user/submissions", params)
 }
 
-func (client JotformAPIClient) GetSubusers() []byte {
+func (client jotformAPIClient) GetSubusers() []byte {
     return client.getRequest("user/subusers", "")
 }
 
-func (client JotformAPIClient) GetFolders() []byte {
+func (client jotformAPIClient) GetFolders() []byte {
     return client.getRequest("user/folders", "")
 }
 
-func (client JotformAPIClient) GetReports() []byte {
+func (client jotformAPIClient) GetReports() []byte {
     return client.getRequest("user/reports", "")
 }
 
-func (client JotformAPIClient) GetSettings() []byte {
+func (client jotformAPIClient) GetSettings() []byte {
     return client.getRequest("user/settings", "")
 }
 
-func (client JotformAPIClient) GetHistory() []byte {
+func (client jotformAPIClient) GetHistory() []byte {
     return client.getRequest("user/history", "")
 }
 
-func (client JotformAPIClient) GetForm(formID int64) []byte {
+func (client jotformAPIClient) GetForm(formID int64) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10), "")
 }
 
-func (client JotformAPIClient) GetFormQuestions(formID int64) []byte {
+func (client jotformAPIClient) GetFormQuestions(formID int64) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/questions", "")
 }
 
-func (client JotformAPIClient) GetFormQuestion(formID int64, qid int) []byte {
+func (client jotformAPIClient) GetFormQuestion(formID int64, qid int) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/question/" + strconv.Itoa(qid), "")
 }
 
-func (client JotformAPIClient) GetFormSubmissions(formID int64, offset string, limit string, filter map[string]string, orderBy string) []byte {
+func (client jotformAPIClient) GetFormSubmissions(formID int64, offset string, limit string, filter map[string]string, orderBy string) []byte {
     var params = createConditions(offset, limit, filter, orderBy)
 
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/submissions", params)
 }
 
-func (client JotformAPIClient) CreateFormSubmissions(formId int64, submission map[string]string) []byte {
+func (client jotformAPIClient) CreateFormSubmissions(formId int64, submission map[string]string) []byte {
     data := make(map[string]string)
 
     for k, _ := range submission {
@@ -238,15 +243,15 @@ func (client JotformAPIClient) CreateFormSubmissions(formId int64, submission ma
     return client.postRequest("form/" + strconv.FormatInt(formId, 10) + "/submissions", data)
 }
 
-func (client JotformAPIClient) GetFormFiles(formID int64) []byte {
+func (client jotformAPIClient) GetFormFiles(formID int64) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/files", "")
 }
 
-func (client JotformAPIClient) GetFormWebhooks(formID int64) []byte {
+func (client jotformAPIClient) GetFormWebhooks(formID int64) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/webhooks", "")
 }
 
-func (client JotformAPIClient) CreateFormWebhook(formId int64, webhookURL string) []byte {
+func (client jotformAPIClient) CreateFormWebhook(formId int64, webhookURL string) []byte {
     params := map[string]string {
         "webhookURL": webhookURL,
     }
@@ -254,31 +259,31 @@ func (client JotformAPIClient) CreateFormWebhook(formId int64, webhookURL string
     return client.postRequest("form/" + strconv.FormatInt(formId, 10) + "/webhooks", params)
 }
 
-func(client JotformAPIClient) GetSubmission(sid int64) []byte {
+func(client jotformAPIClient) GetSubmission(sid int64) []byte {
     return client.getRequest("user/submission/" + strconv.FormatInt(sid, 10), "")
 }
 
-func(client JotformAPIClient) GetReport(reportID int64) []byte {
+func(client jotformAPIClient) GetReport(reportID int64) []byte {
     return client.getRequest("user/report/" + strconv.FormatInt(reportID, 10), "")
 }
 
-func (client JotformAPIClient) GetFolder(folderID int64) []byte {
+func (client jotformAPIClient) GetFolder(folderID int64) []byte {
     return client.getRequest("user/folder/" + strconv.FormatInt(folderID, 10), "")
 }
 
-func (client JotformAPIClient) GetFormProperties(formID int64) []byte {
+func (client jotformAPIClient) GetFormProperties(formID int64) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/properties", "")
 }
 
-func (client JotformAPIClient) GetFormProperty(formID int64, propertyKey string) []byte {
+func (client jotformAPIClient) GetFormProperty(formID int64, propertyKey string) []byte {
     return client.getRequest("form/" + strconv.FormatInt(formID, 10) + "/properties/" + propertyKey, "")
 }
 
-func (client JotformAPIClient) DeleteSubmission(sid int64) []byte {
+func (client jotformAPIClient) DeleteSubmission(sid int64) []byte {
     return client.deleteRequest("submission/" + strconv.FormatInt(sid, 10))
 }
 
-func (client JotformAPIClient) EditSubmission(sid int64, submission map[string]string) []byte {
+func (client jotformAPIClient) EditSubmission(sid int64, submission map[string]string) []byte {
     data := make(map[string]string)
 
     for k, _ := range submission {
@@ -292,11 +297,11 @@ func (client JotformAPIClient) EditSubmission(sid int64, submission map[string]s
     return client.postRequest("submission/" + strconv.FormatInt(sid, 10), data)
 }
 
-func (client JotformAPIClient) CloneForm(formID int64) []byte {
+func (client jotformAPIClient) CloneForm(formID int64) []byte {
     return client.postRequest("form/" + strconv.FormatInt(formID, 10) + "/clone", nil)
 }
 
-func (client JotformAPIClient) DeleteFormQuestion(formID int64, qid int) []byte {
+func (client jotformAPIClient) DeleteFormQuestion(formID int64, qid int) []byte {
     return client.deleteRequest("form/" + strconv.FormatInt(formID, 10) + "/question/" + strconv.Itoa(qid))
 }
 
